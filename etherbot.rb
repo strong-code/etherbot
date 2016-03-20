1d6d4ad892e730817179a76a455952763131e09e
clindsay107 <clindsay107@gmail.com>
2016-03-20 00:55:30 -0700

    make shitty code slightly less shitty, but still really bad. Add address.yml

--- etherbot.rb
+++ etherbot.rb
@@ -3,48 +3,92 @@ require 'open-uri'
 require 'json'
 require 'socket'
 require 'bigdecimal'
+require 'yaml'
 
-def get_price
-  response = JSON.parse(open('https://poloniex.com/public?command=returnTicker').read)
-  account = JSON.parse(open('http://api.etherscan.io/api?module=account&action=balance&address=0x7b30b6675e44c31c51bb4771f983f2671121e745&tag=latest').read)
-  btc = response['BTC_ETH']['last'].slice(0,7)
-  btc_usd = response['USDT_BTC']['last'].slice(0,7)
-  usd = BigDecimal(btc) * BigDecimal(btc_usd)
-  balance = account['result'].slice(0,3)
-  earnings = BigDecimal(usd) * BigDecimal(balance)
-  returns = (earnings.to_f / 1480.to_f) * 100
-  volume = response['BTC_ETH']['baseVolume']
-  return "[BTC] #{btc} | [USD] $#{usd.to_f.round(2)} | [Volume] #{volume.to_f.round(2)} |  BP's balance: #{balance} ETH - earnings: $#{earnings.to_i} - #{returns.round}% return"
+def get_user_info(nick)
+  user = YAML.load_file('./address.yml')[nick]
+  return say("No address info for #{nick}")
+
+  address = user['addr']
+  investment = user['investment']
+  url  = "http://api.etherscan.io/api?module=account&action=balance&address=#{address}&tag=latest"
+  json = JSON.parse(open(url).read)
+  balance = json['result'].slice(0, 3)
+
+  user_data = {
+    :balance => balance,
+    :investment =>  investment
+  }
 end
 
-def run()
+def get_price_info
+  url     = 'https://poloniex.com/public?command=returnTicker'
+  json    = JSON.parse(open(url).read)
+
+  btc        = json['BTC_ETH']['last'].slice(0, 7)
+  btc_usd    = json['USDT_BTC']['last'].slice(0, 7)
+  eth_volume = json['BTC_ETH']['baseVolume'].to_f
+  eth_usd    = BigDecimal(btc) * BigDecimal(btc_usd)
+
+  results = {
+    :btc => btc,
+    :eth_usd => eth_usd,
+    :volume => eth_volume,
+  }
+end
+
+def say(msg)
+  @socket.puts("PRIVMSG #{@chan} :#{msg}\n")
+end
+
+def run(verbose = false, test_mode = false)
   nick = 'etherb0t'
-  chan = '#lifting'
+  @chan = (test_mode ? '#etherbottest' : '#lifting')
   server = 'irc.rizon.net'
   port = 6667
 
-  socket = TCPSocket.open(server, port)
+  @socket = TCPSocket.open(server, port)
 
-  socket.puts "USER #{nick} 0 * #{nick}\n"
-  socket.puts "NICK #{nick}\n"
+  @socket.puts "USER #{nick} 0 * #{nick}\n"
+  @socket.puts "NICK #{nick}\n"
 
   sleep(1)
 
-  socket.puts "JOIN #{chan}\n"
+  @socket.puts "JOIN #{@chan}\n"
 
   @count = 0
 
-  until socket.eof? do
-    msg = socket.gets
-    puts msg
+  until @socket.eof? do
+    msg = @socket.gets
+    puts msg if verbose
 
     if msg.include?('PING')
-      socket.puts "PONG :pingis\n"
+      @socket.puts "PONG :pingis\n"
     elsif msg.include?('!eth')
-      socket.puts "PRIVMSG #{chan} :#{get_price}\n"
+      prices = get_price_info
+      usd    = prices[:eth_usd].to_f.round(2)
+      volume = prices[:volume].round(2)
+      btc    = prices[:btc]
+
+      say("BTC: #{btc} | USD: $#{usd} | Volume: #{volume}")
+    elsif msg.include?('!myeth')
+      parts      = msg.split
+      nick       = parts[0].match(/^:(.*)\!~/)[1]
+      user_data  = get_user_info(nick)
+      next if !user_data
+
+      balance    = user_data[:balance]
+      investment = user_data[:investment]
+      prices     = get_price_info
+      earnings   = (balance.to_f * prices[:eth_usd].to_f).round
+      returns    = ((earnings / investment.to_f) * 100).round
+
+      say("Balance: #{balance} | Worth: $#{earnings} USD | Return: %#{returns}")
     end
   end
 
 end
 
-run()
+verbose = ARGV[0] || false
+test_mode = ARGV[1] || false
+run(verbose, test_mode)
